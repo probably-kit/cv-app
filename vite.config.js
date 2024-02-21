308ea6b11b4b8d6d5e24132871d8488e6216405b
probably-kit <probably.kit@gmail.com>
2024-02-21 22:37:54 +0100

    Add dependencies and icons

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgrPlugin(), react()],
   base: '/cv-app/'
 })
