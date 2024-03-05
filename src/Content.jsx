0fb9edde10701650d755c70f4c91ad7d4183d5c1
probably-kit <probably.kit@gmail.com>
2024-03-05 22:29:27 +0100

    Create grid

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,8 +6,7 @@ function Content({ personalInfo, education, experience }) {
     <div>
       <h1>Content</h1>
       <div className='content'>
-        <div className="personal-info">
-          {/* {personalInfo.map((item, index) => (
+        {/* {personalInfo.map((item, index) => (
             <div className='personal-info-container' key={index}>
               <p className='name'>{item.Name}</p> 
               <p> {item.Surname}</p>
@@ -19,58 +18,47 @@ function Content({ personalInfo, education, experience }) {
               </div>
             </div>
           ))} */}
-          <div className='container'>
-            <p className='name'>Mikita</p>
-            <p className='surname'>Lohinau</p>
-            <p className='position'>Full Stack Developer</p>
-          </div>
-          <div className='line'></div>
-          <div className='container row'>
-            <div className='container'>
-              <p>CONTACT</p>
-              <p>+48516557455</p>
-              <p>mikita.lohinau@gmail.com</p>
-              <p>Gdansk, Poland</p>
-              <p>Github</p>
-            </div>
-            <div className='line rotated'></div>
-            <div className='container'>
-              <p>Summary</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quod eum officia possimus repellat blanditiis incidunt illum sed minus ipsam beatae iure culpa fugit, earum quidem numquam hic reiciendis quia similique dignissimos esse accusamus, alias aliquam? Culpa, obcaecati.</p>
-            </div>
-          </div>
-          <div className='line'></div>
-          <div className='container row'>
-            <div className='container'>
-              <div className="container">
-                <p>SKILLS</p>
-                <p>HTML</p>
-                <p>CSS</p>
-                <p>JavaScript</p>
-                <p>React</p>
-                <p>Node.js</p>
-                <p>Express</p>
-                <p>MongoDB</p>
-                <p>MySQL</p>
-              </div>
-              <div className="container">
-                <p>Education</p>
-                <p>Belarusian State University of Informatics and Radioelectronics</p>
-                <p>Software Engeneer</p>
-                <p>2020-2023</p>
-              </div>
-            </div>
-            <div className='container'>
-              <p>Working experience</p>
-              <div className='container row'>
-                <p>Security Lab</p>
-                <p>2022-2023</p>
-              </div>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat optio corporis, atque distinctio similique quam libero minima, veniam quibusdam accusantium corrupti ullam quasi quos iste earum amet aspernatur tenetur porro maiores alias soluta eum neque? Vitae possimus nemo magni provident ad sed earum eius sint illo! Vero, aliquam. Odit placeat delectus sapiente magni laudantium accusamus deleniti, commodi qui mollitia nobis eum temporibus culpa at doloremque.</p>
-            </div>
-          </div>
+        <div className='content-item span-5'>
+          <p className='name'>Mikita</p>
+          <p className='name'>Lohinau</p>
+          <p className='position'>Full Stack Developer</p>
         </div>
-        {/* <div className="experience">
+          <div className='content-item span-2'>
+            <p className='title'>CONTACT</p>
+            <p>+48516557455</p>
+            <p>mikita.lohinau@gmail.com</p>
+            <p>Gdansk, Poland</p>
+            <p>Github</p>
+          </div>
+          <div className='content-item span-3'>
+            <p className='title'>Summary</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quod eum officia possimus repellat blanditiis incidunt illum sed minus ipsam beatae iure culpa fugit, earum quidem numquam hic reiciendis quia similique dignissimos esse accusamus, alias aliquam? Culpa, obcaecati.</p>
+          </div>       
+            <div className="content-item">
+              <p className='title'>SKILLS</p>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>JavaScript</p>
+              <p>React</p>
+              <p>Node.js</p>
+              <p>Express</p>
+              <p>MongoDB</p>
+              <p>MySQL</p>
+            </div>
+          <div className='content-item span-4'>
+            <p className='title'>Working experience</p>
+              <p>Security Lab</p>
+              <p>2022-2023</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat optio corporis, atque distinctio similique quam libero minima, veniam quibusdam accusantium corrupti ullam quasi quos iste earum amet aspernatur tenetur porro maiores alias soluta eum neque? Vitae possimus nemo magni provident ad sed earum eius sint illo! Vero, aliquam. Odit placeat delectus sapiente magni laudantium accusamus deleniti, commodi qui mollitia nobis eum temporibus culpa at doloremque.</p>
+          </div>
+          <div className="content-item">
+              <p className='title'>Education</p>
+              <p>Belarusian State University of Informatics and Radioelectronics</p>
+              <p>Software Engeneer</p>
+              <p>2020-2023</p>
+            </div>
+      </div>
+      {/* <div className="experience">
           <h2>Experience</h2>
           {experience.map((item, index) => (
             <div className='experience-container' key={index}>
@@ -99,7 +87,6 @@ function Content({ personalInfo, education, experience }) {
             </div>
           ))}
          </div> */}
-      </div>
     </div>
   );
 }
