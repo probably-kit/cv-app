bef74524ea17d07295c0d41084e216551069030f
probably-kit <probably.kit@gmail.com>
2024-03-10 22:33:52 +0100

    add generate pdf button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 import { Dropdown, DropdownFormItem } from './Dropdown.jsx';
 import Content from './Content.jsx';
-import { useState } from 'react';
-
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 function App() {
-  const [isContentVisible, setIsContentVisible] = useState(true);
   const [formData, setFormData] = useState({
     personalInfo: [],
     education: [],
@@ -20,20 +20,73 @@ function App() {
     }));
   };
 
-//   const toggleVisibility = (index) => {
-//     const updatedData = [...submittedData];
-//     updatedData[index] = {
-//         ...updatedData[index],
-//         visible: !updatedData[index].visible,
-//     };
-//     setSubmittedData(updatedData);
-// };
+  const generatePdfFromContent = () => {
+    const docDefinition = {
+      content: [
+        // Directly using static data for demonstration; adjust according to your actual data structure
+        { text: 'Mikita Lohinau', style: 'header' },
+        { text: 'Full Stack Developer', style: 'subheader' },
+        {
+          canvas: [
+            { type: 'line', x1: 0, y1: 5, x2: 515, y2: 5, lineWidth: 2 }
+          ],
+          margin: [0, 10]
+        },
+        {
+          columns: [
+            {
+              width: '*',
+              text: 'Contact',
+              style: 'sectionHeader'
+            },
+            {
+              width: 'auto',
+              text: [
+                { text: '+48516557455\n', margin: [0, 5] },
+                { text: 'mikita.lohinau@gmail.com\n', margin: [0, 5] },
+                { text: 'Gdansk, Poland\n', margin: [0, 5] },
+                { text: 'Github: ', link: 'https://github.com/probably-kit', decoration: 'underline', color: 'blue' },
+                { text: '\nLinkedIn: ', link: 'https://www.linkedin.com/in/mikita-lohinau-0a5239235/', decoration: 'underline', color: 'blue' }
+              ],
+              margin: [0, 5]
+            }
+          ]
+        },
+        // More content following your structure
+      ],
+      styles: {
+        header: {
+          fontSize: 22,
+          bold: true,
+          margin: [0, 0, 0, 10],
+          alignment: 'center'
+        },
+        subheader: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10]
+        },
+        sectionHeader: {
+          fontSize: 14,
+          bold: true,
+          decoration: 'underline',
+          margin: [0, 10, 0, 5]
+        },
+        // Add more styles as needed
+      },
+      defaultStyle: {
+        columnGap: 20,
+      }
+    };
+
+    pdfMake.createPdf(docDefinition).download("CV_Mikita_Lohinau.pdf");
+  };
 
   return (
     <>
       <div className='main-container'>
         <div className='form-container'>
-        <h1>CV creator</h1>
+          <h1>CV creator</h1>
           <Dropdown onFormDataChange={data => handleFormDataChange('personalInfo', data)} title="Personal Information">
             <DropdownFormItem inputTitle='Name' />
             <DropdownFormItem inputTitle='Surname' />
@@ -51,16 +104,16 @@ function App() {
           <Dropdown onFormDataChange={data => handleFormDataChange('experience', data)} title="Expirience" firstFormLabel="Company Name" secondFormLabel="Position Title" >
             <DropdownFormItem inputTitle='Company Name' />
             <DropdownFormItem inputTitle="Position Title" />
-            <DropdownFormItem inputTitle='Description'  />
+            <DropdownFormItem inputTitle='Description' />
             <DropdownFormItem inputTitle="Start date" type="date" containerClassName="dropdown-form-item unspan" />
             <DropdownFormItem inputTitle="End date" type="date" containerClassName="dropdown-form-item unspan" />
           </Dropdown>
+          <button onClick={generatePdfFromContent}>Download PDF</button>
         </div>
-        <Content className='content'  {...formData} isVisible={true} />
+        <Content {...formData} isVisible={true} />
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Content.jsx
+++ src/Content.jsx
@@ -6,88 +6,47 @@ function Content({ personalInfo, education, experience }) {
     <div>
       <h1>Content</h1>
       <div className='content'>
-        {/* {personalInfo.map((item, index) => (
-            <div className='personal-info-container' key={index}>
-              <p className='name'>{item.Name}</p> 
-              <p> {item.Surname}</p>
-              <p>{item.Position}</p>
-              <div className='personal-info-items'>
-                <p>{item.Phone}</p>
-                <p>{item.Email}</p>
-                <p>{item.Links}</p>
-              </div>
-            </div>
-          ))} */}
         <div className='content-item span-5'>
           <p className='name'>Mikita</p>
           <p className='name'>Lohinau</p>
           <p className='position'>Full Stack Developer</p>
         </div>
-          <div className='content-item span-2'>
-            <p className='title'>CONTACT</p>
-            <p>+48516557455</p>
-            <p>mikita.lohinau@gmail.com</p>
-            <p>Gdansk, Poland</p>
-            <a href='https://github.com/probably-kit'>Github</a>
-            <a href='https://www.linkedin.com/in/mikita-lohinau-0a5239235/'>LinkedIn</a>
-          </div>
-          <div className='content-item span-3'>
-            <p className='title'>Summary</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quod eum officia possimus repellat blanditiis incidunt illum sed minus ipsam beatae iure culpa fugit, earum quidem numquam hic reiciendis quia similique dignissimos esse accusamus, alias aliquam? Culpa, obcaecati.</p>
-          </div>       
-            <div className="content-item span-1">
-              <p className='title'>SKILLS</p>
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>JavaScript</p>
-              <p>React</p>
-              <p>Node.js</p>
-              <p>Express</p>
-              <p>MongoDB</p>
-              <p>MySQL</p>
-            </div>
-          <div className='content-item span-4'>
-            <p className='title'>Working experience</p>
-              <p>Security Lab</p>
-              <p>2022-2023</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat optio corporis, atque distinctio similique quam libero minima, veniam quibusdam accusantium corrupti ullam quasi quos iste earum amet aspernatur tenetur porro maiores alias soluta eum neque? Vitae possimus nemo magni provident ad sed earum eius sint illo! Vero, aliquam. Odit placeat delectus sapiente magni laudantium accusamus deleniti, commodi qui mollitia nobis eum temporibus culpa at doloremque.</p>
-          </div>
-          <div className="content-item">
-              <p className='title'>Education</p>
-              <p>Belarusian State University of Informatics and Radioelectronics</p>
-              <p>Software Engeneer</p>
-              <p>2020-2023</p>
-            </div>
-      </div>
-      {/* <div className="experience">
-          <h2>Experience</h2>
-          {experience.map((item, index) => (
-            <div className='experience-container' key={index}>
-              <div className='experience-item'>
-                <p>Start Date: {item["Start date"]}</p>
-                <p>End Date: {item["End date"]}</p>
-              </div>
-              <div className='experience-item'>
-                <h2 className='company-name'>{item['Company Name']}</h2>
-                <p className='position-title'>{item['Position Title']}</p>
-                <p className='description'>{item.Description}</p>
-              </div>
-
-            </div>
-          ))}
+        <div className='content-item span-2'>
+          <p className='title'>CONTACT</p>
+          <p>+48516557455</p>
+          <p>mikita.lohinau@gmail.com</p>
+          <p>Gdansk, Poland</p>
+          <a href='https://github.com/probably-kit'>Github</a>
+          <a href='https://www.linkedin.com/in/mikita-lohinau-0a5239235/'>LinkedIn</a>
         </div>
-         
-        <div className="education">
-          <h2>Education</h2>
-          {education.map((item, index) => (
-            <div key={index}>
-              <h2>{item.School}</h2> 
-              <p>{item.Degree}</p>
-              <p>Start Date: {item["Start date"]}</p> 
-              <p>End Date: {item["End date"]}</p>
-            </div>
-          ))}
-         </div> */}
+        <div className='content-item span-3'>
+          <p className='title'>Summary</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quod eum officia possimus repellat blanditiis incidunt illum sed minus ipsam beatae iure culpa fugit, earum quidem numquam hic reiciendis quia similique dignissimos esse accusamus, alias aliquam? Culpa, obcaecati.</p>
+        </div>
+        <div className="content-item span-1">
+          <p className='title'>SKILLS</p>
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>JavaScript</p>
+          <p>React</p>
+          <p>Node.js</p>
+          <p>Express</p>
+          <p>MongoDB</p>
+          <p>MySQL</p>
+        </div>
+        <div className='content-item span-4'>
+          <p className='title'>Working experience</p>
+          <p>Security Lab</p>
+          <p>2022-2023</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat optio corporis, atque distinctio similique quam libero minima, veniam quibusdam accusantium corrupti ullam quasi quos iste earum amet aspernatur tenetur porro maiores alias soluta eum neque? Vitae possimus nemo magni provident ad sed earum eius sint illo! Vero, aliquam. Odit placeat delectus sapiente magni laudantium accusamus deleniti, commodi qui mollitia nobis eum temporibus culpa at doloremque.</p>
+        </div>
+        <div className="content-item">
+          <p className='title'>Education</p>
+          <p>Belarusian State University of Informatics and Radioelectronics</p>
+          <p>Software Engeneer</p>
+          <p>2020-2023</p>
+        </div>
+      </div>
     </div>
   );
 }
